84bf3f59784b5cdabaae7d280faa12c24cc003a0
i3h <noobly314@gmail.com>
2020-05-20 08:27:02 +0000

    update flex css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,7 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  widthLevel: string;
-
   constructor(private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    if (window.innerWidth < 770) {
-      this.widthLevel = 'min';
-    } else if (window.innerWidth < 1340) {
-      this.widthLevel = 'mid';
-    } else {
-      this.widthLevel = 'max';
-    }
-    console.log(this.widthLevel);
-  }
+  ngOnInit() {}
 }
