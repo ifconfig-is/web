52138415b63d138d9c8c2bca4ec6e4dd77d36838
i3h <noobly314@gmail.com>
2021-07-28 17:19:31 +0800

    get api url from environment

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { environment } from '../environments/environment';
 
-const uri = '/gql'; // <-- add the URL of the GraphQL server here
+//const uri = '/gql';
+const uri = environment.apiUrl;
+
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({ uri }),
     cache: new InMemoryCache(),
     defaultOptions: {
       watchQuery: {
-        errorPolicy: 'all'
-      }
-    }
+        errorPolicy: 'all',
+      },
+    },
   };
 }
 
@@ -22,8 +25,8 @@ export function createApollo(httpLink: HttpLink) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink]
-    }
-  ]
+      deps: [HttpLink],
+    },
+  ],
 })
 export class GraphQLModule {}
