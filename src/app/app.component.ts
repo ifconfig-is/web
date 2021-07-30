95de3a9f8cd7721ab97113ff1f2b190c6c75ed92
i3h <noobly314@gmail.com>
2021-07-30 23:04:52 +0800

    finish mobile style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  isMobile: boolean = window.innerWidth < 768;
+
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {}
